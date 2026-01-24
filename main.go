149f787a603c2b5c2ce74ff9543cc07af66813bf
Michael Luu <67599521+kemonprogrammer@users.noreply.github.com>
2026-01-24 17:10:08 +0100

    Filter successful deployments

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	// setup github
 	owner := "kemonprogrammer"
-	repo := "kiali"
+	repo := "github-go-client"
 	githubPat := os.Getenv("GITHUB_PAT")
 	ctx := context.Background()
 	client := github.NewClient(nil).WithAuthToken(githubPat)
@@ -23,24 +23,33 @@ func main() {
 		ListOptions: github.ListOptions{},
 	})
 
-	for i, d := range deployments {
+	for i, d := range filterSuccessfulDeployments(client, ctx, owner, repo, deployments) {
 		fmt.Printf("\nDeployment %d:\n", i)
 
 		fmt.Printf("sha: %s\n", d.GetSHA())
 		fmt.Printf("id: %d\n", d.GetID())
 		fmt.Printf("created at: %s\n", d.GetCreatedAt())
+	}
+
+	// read deployments
+
+	//orgs, _, _ := client.Organizations.List(context.Background(), "willnorris", nil)
+}
+
+func filterSuccessfulDeployments(client *github.Client, ctx context.Context, owner string, repo string, deployments []*github.Deployment) []*github.Deployment {
+	var result []*github.Deployment
+
+	for _, d := range deployments {
 
 		statuses, _, _ := client.Repositories.ListDeploymentStatuses(ctx, owner, repo, d.GetID(), &github.ListOptions{
 			Page:    0,
 			PerPage: 10,
 		})
 		for _, status := range statuses {
-			fmt.Printf("status state: %s\n", status.GetState())
+			if status.GetState() == "success" {
+				result = append(result, d)
+			}
 		}
 	}
-
-	// read deployments
-
-	//orgs, _, _ := client.Organizations.List(context.Background(), "willnorris", nil)
-
+	return result
 }
