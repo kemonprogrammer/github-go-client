d0cf5f67fe3506d89ed27d1905e31d15cb44dfbb
Michael Luu <67599521+kemonprogrammer@users.noreply.github.com>
2026-01-24 02:58:23 +0100

    Get deployments

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/kemonprogrammer/github-go-client
 
 go 1.25.6
+
+require github.com/google/go-github/v81 v81.0.0
+
+require github.com/google/go-querystring v1.1.0 // indirect
--- main.go
+++ main.go
@@ -1,4 +1,46 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"github.com/google/go-github/v81/github"
+	"os"
+)
+
 func main() {
+	// setup github
+	owner := "kemonprogrammer"
+	repo := "kiali"
+	githubPat := os.Getenv("GITHUB_PAT")
+	ctx := context.Background()
+	client := github.NewClient(nil).WithAuthToken(githubPat)
+
+	deployments, _, _ := client.Repositories.ListDeployments(ctx, owner, repo, &github.DeploymentsListOptions{
+		SHA:         "",
+		Ref:         "",
+		Task:        "",
+		Environment: "",
+		ListOptions: github.ListOptions{},
+	})
+
+	for i, d := range deployments {
+		fmt.Printf("\nDeployment %d:\n", i)
+
+		fmt.Printf("sha: %s\n", d.GetSHA())
+		fmt.Printf("id: %d\n", d.GetID())
+		fmt.Printf("created at: %s\n", d.GetCreatedAt())
+
+		statuses, _, _ := client.Repositories.ListDeploymentStatuses(ctx, owner, repo, d.GetID(), &github.ListOptions{
+			Page:    0,
+			PerPage: 10,
+		})
+		for _, status := range statuses {
+			fmt.Printf("status state: %s\n", status.GetState())
+		}
+	}
+
+	// read deployments
+
+	//orgs, _, _ := client.Organizations.List(context.Background(), "willnorris", nil)
+
 }
